b88cad44f55ffcad613e9a20116e2171b4bb2e56
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2024-04-21 13:09:07 +0300

    Finished the Welcome page; Added search bar; Added job type tabs;

--- client/JobIT/app/_layout.js
+++ client/JobIT/app/_layout.js
@@ -1,7 +1,32 @@
-import {Stack} from 'expo-router'
+import React, { useCallback, useEffect } from 'react'
+import { View, Text } from 'react-native'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { Stack } from 'expo-router'
 
 const Layout = () => {
-    return <Stack/>
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            console.log('Fonts loaded, hiding splash screen');
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        console.log('Fonts not loaded, returning null');
+        return null;
+    }
+
+    console.log('Rendering View component');
+    return (
+        <Stack onLayout={onLayoutRootView}/>
+    );
 }
 
 export default Layout;
