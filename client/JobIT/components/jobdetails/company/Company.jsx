8e9c522554f15f12b21326e8eafcd5e959f71ccb
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2024-04-21 15:29:14 +0300

    Started creating the job details page

--- client/JobIT/components/jobdetails/company/Company.jsx
+++ client/JobIT/components/jobdetails/company/Company.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 
 import styles from './company.style'
+import { icons } from '../../../constants'
+import { checkImageURL } from '../../../utilities'
 
-const Company = () => {
+const Company = ({ companyLogo, jobTitle, companyName, location }) => {
   return (
-    <View>
-      <Text>Company</Text>
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+        <Image source={{
+          uri: checkImageURL(companyLogo)
+            ? companyLogo
+            : 'https://jobs.nsbs.bg/root/images/default_image_company.png'
+        }}
+          style={styles.logoImage} />
+      </View>
+      <View style={styles.jobTitleBox}>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}>{companyName} | </Text>
+        <View style={styles.locationBox}>
+          <Image source={icons.location}
+            resizeMode='contain'
+            style={styles.locationImage} />
+            <Text style={styles.locationName}>{location}</Text>
+        </View>
+      </View>
     </View>
   )
 }
