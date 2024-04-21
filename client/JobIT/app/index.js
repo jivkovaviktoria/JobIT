107373fba7935507196a679bf08995df58994afd
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2024-04-21 11:46:09 +0300

    Created the header of the app

--- client/JobIT/app/index.js
+++ client/JobIT/app/index.js
@@ -1,19 +1,38 @@
-import {useState} from 'react'
-import {View, ScrollView, SafeAreaView} from 'react-native';
-import {Stack, useRouter} from 'expo-router';
+import { useState } from 'react'
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
 
-import {COLORS, icons, images, SIZES} from '../constants';
+import { COLORS, icons, images, SIZES } from '../constants';
 import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
 
 const Home = () => {
     const router = useRouter();
 
     return (
-        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
-            <Stack.Screen options={{headerStyle: {backgroundColor: COLORS.lightWhite}}}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                    ),
+                    headerTitle: ""
+                }}>
 
             </Stack.Screen>
-        </SafeAreaView>
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{flex: 1, padding: SIZES.medium}}>
+                    <Welcome/>
+                    <Popularjobs/>
+                    <Nearbyjobs/>
+                </View>
+            </ScrollView>
+        </SafeAreaView >
     );
 }
 
